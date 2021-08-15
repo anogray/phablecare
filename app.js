c37a9251a2d69204219b69ab6cfbe689748415ea
anogray <bbncr97@gmail.com>
2021-08-15 14:00:18 +0530

    check

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
+
 dotenv.config();
 //app config
 const app = express();
 const PORT = process.env.PORT || 8001;
-import cors from "cors";
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    return res.status(200).send({"msg":"server accessible"});
+})
+
 //middleware
 // app.use(express.json());
 //db config
 //api endpoints
-import routes from "./routes/v1/route.js";
-app.use("/routes/api/v1", routes);
+// import routes from "./routes/v1/route.js";
+// app.use("/routes/api/v1", routes);
 
 //listener
 app.listen(PORT, () => console.log(`server running at port ${PORT}`));
